21a21f708a49e073095b8ba8f29210fd1cb31f57
michaelwiderin <michael.widerin@hotmail.com>
2022-05-29 19:49:10 +0200

    added day 2 results

---
+++ src/AdventOfCode.Cli/Day2/Day2.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode.Cli.Day2
+{
+    public class Day2
+    {
+        public static void RunPartOne()
+        {
+            var input = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Day2", "Input.txt"));
+            var lines = input.Split("\r\n");
+
+            var horizontal = 0;
+            var depth = 0;
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(' ');
+
+                switch (values[0])
+                {
+                    case "forward":
+                        horizontal += Convert.ToInt32(values[1]);
+                        break;
+                    case "up":
+                        depth -= Convert.ToInt32(values[1]);
+                        break;
+                    case "down":
+                        depth += Convert.ToInt32(values[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Results - Part One: {horizontal * depth}");
+        }
+
+        public static void RunPartTwo()
+        {
+            var input = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Day2", "Input.txt"));
+            var lines = input.Split("\r\n");
+
+            var horizontal = 0;
+            var depth = 0;
+            var aim = 0;
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(' ');
+
+                switch (values[0])
+                {
+                    case "forward":
+                        horizontal += Convert.ToInt32(values[1]);
+                        depth += aim * Convert.ToInt32(values[1]);
+                        break;
+                    case "up":
+                        aim -= Convert.ToInt32(values[1]);
+                        break;
+                    case "down":
+                        aim += Convert.ToInt32(values[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Results - Part Two: {horizontal * depth}");
+        }
+    }
+}
--- src/AdventOfCode.Cli/Program.cs
+++ src/AdventOfCode.Cli/Program.cs
@@ -1,8 +1,18 @@
 ﻿using AdventOfCode.Cli.Day1;
+using AdventOfCode.Cli.Day2;
+using AdventOfCode.Cli.Day6;
 
+Console.WriteLine("========================================");
+Console.WriteLine("Day 1:");
 Day1.RunPartOne();
 Day1.RunPartTwo();
-
-//Day6.Run();
+Console.WriteLine("========================================");
+Console.WriteLine("Day 2:");
+Day2.RunPartOne();
+Day2.RunPartTwo();
+Console.WriteLine("========================================");
+Console.WriteLine("Day 6:");
+Day6.Run();
+Console.WriteLine("========================================");
 
 Console.ReadKey();
